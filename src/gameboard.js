05ca92b4fa81cd2786055ccb68c3f707fb6c41a0
deepend15 <170750487+deepend15@users.noreply.github.com>
2025-07-06 14:08:15 -0400

    Add placeShips() and clearBoard() fns & tests

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,4 +1,4 @@
-export const gameboard = (function() {
+export const gameboard = (function () {
   const rows = 10;
   const columns = 10;
   const board = [];
@@ -10,7 +10,88 @@ export const gameboard = (function() {
     }
   }
 
+  // const getBoard = () => console.log(board);
+
   const getBoard = () => board;
 
-  return { getBoard }
+  function placeShip(ship, firstCoordinate, secondCoordinate) {
+    const coordinates = [
+      firstCoordinate[0],
+      firstCoordinate[1],
+      secondCoordinate[0],
+      secondCoordinate[1],
+    ];
+
+    coordinates.forEach((coordinate) => {
+      if (coordinate < 0 || coordinate > 9) {
+        throw new Error("Invalid coordinates");
+      }
+    });
+
+    if (
+      firstCoordinate[0] !== secondCoordinate[0] &&
+      firstCoordinate[1] !== secondCoordinate[1]
+    ) {
+      throw new Error("Invalid coordinates");
+    }
+
+    if (firstCoordinate[0] === secondCoordinate[0]) {
+      if (
+        !(
+          secondCoordinate[1] - firstCoordinate[1] === ship.length - 1 ||
+          (secondCoordinate[1] - firstCoordinate[1]) * -1 === ship.length - 1
+        )
+      ) {
+        throw new Error("Invalid coordinates");
+      }
+    }
+
+    if (firstCoordinate[1] === secondCoordinate[1]) {
+      if (
+        !(
+          secondCoordinate[0] - firstCoordinate[0] === ship.length - 1 ||
+          (secondCoordinate[0] - firstCoordinate[0]) * -1 === ship.length - 1
+        )
+      ) {
+        throw new Error("Invalid coordinates");
+      }
+    }
+
+    const board = gameboard.getBoard();
+
+    const squares = [firstCoordinate];
+
+    let x = firstCoordinate[0];
+    let y = firstCoordinate[1];
+
+    for (let i = 1; i < ship.length - 1; i++) {
+      if (firstCoordinate[0] === secondCoordinate[0]) {
+        if (firstCoordinate[1] < secondCoordinate[1]) y += 1;
+        else y -= 1;
+      } else {
+        if (firstCoordinate[0] < secondCoordinate[0]) x += 1;
+        else x -= 1;
+      }
+      squares.push([x, y]);
+    }
+
+    squares.push(secondCoordinate);
+
+    squares.forEach((square) => {
+      if (board[square[0]][square[1]] !== null) {
+        throw new Error("Coordinates already occupied!");
+      }
+      board[square[0]][square[1]] = ship.id;
+    });
+  }
+
+  function clearBoard() {
+    board.forEach((row) => {
+      for (let i = 0; i < row.length; i++) {
+        row[i] = null;
+      }
+    });
+  }
+
+  return { getBoard, placeShip, clearBoard };
 })();
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,10 +1,107 @@
-import { expect, test } from "@jest/globals";
+import { expect, test, afterEach } from "@jest/globals";
 import { gameboard } from "./gameboard.js";
+import { ship } from "./ship.js";
+
+afterEach(() => {
+  gameboard.clearBoard();
+});
 
 test("gameboard.getBoard() returns an array with 10 rows and 10 columns", () => {
   const board = gameboard.getBoard();
   expect(board.length).toBe(10);
   board.forEach((row) => {
     expect(row.length).toBe(10);
+  });
+});
+
+test("gameboard.placeShip() places a ship on the gameboard in a row", () => {
+  const selectedShipArray = ship.getShips().filter((ship) => ship.id === "5");
+  const selectedShip = selectedShipArray[0];
+  gameboard.placeShip(selectedShip, [0, 0], [0, 4]);
+  const board = gameboard.getBoard();
+  const firstRow = board[0];
+  const firstFiveSquares = firstRow.slice(0, 5);
+  firstFiveSquares.forEach((square) => {
+    expect(square).toBe("5");
+  });
+  const secondShipArray = ship.getShips().filter((ship) => ship.id === "2");
+  const secondShip = secondShipArray[0];
+  gameboard.placeShip(secondShip, [4, 4], [4, 5]);
+  expect(board[4][4]).toBe("2");
+  expect(board[4][5]).toBe("2");
+});
+
+test("gameboard.placeShip() places a ship on the gameboard in a column", () => {
+  const selectedShipArray = ship.getShips().filter((ship) => ship.id === "5");
+  const selectedShip = selectedShipArray[0];
+  gameboard.placeShip(selectedShip, [0, 0], [4, 0]);
+  const board = gameboard.getBoard();
+  const firstColumn = [];
+  board.forEach((row) => {
+    firstColumn.push(row[0]);
+  });
+  const firstFiveColumnSquares = firstColumn.slice(0, 5);
+  firstFiveColumnSquares.forEach((square) => {
+    expect(square).toBe("5");
+  });
+  const secondShipArray = ship.getShips().filter((ship) => ship.id === "2");
+  const secondShip = secondShipArray[0];
+  gameboard.placeShip(secondShip, [4, 4], [5, 4]);
+  expect(board[4][4]).toBe("2");
+  expect(board[5][4]).toBe("2");
+});
+
+test("gameboard.placeShip() does not allow a new ship to be placed on an area that is already occupied by a ship", () => {
+  const selectedShipArray = ship.getShips().filter((ship) => ship.id === "5");
+  const selectedShip = selectedShipArray[0];
+  gameboard.placeShip(selectedShip, [0, 0], [0, 4]);
+  const secondShipArray = ship.getShips().filter((ship) => ship.id === "2");
+  const secondShip = secondShipArray[0];
+  expect(() => gameboard.placeShip(secondShip, [0, 0], [1, 0])).toThrow;
+});
+
+test("gameboard.placeShip() does not allow coordinates off of the board", () => {
+  const selectedShipArray = ship.getShips().filter((ship) => ship.id === "5");
+  const selectedShip = selectedShipArray[0];
+  expect(() => gameboard.placeShip(selectedShip, [-1, 0], [0, 0])).toThrow;
+  expect(() => gameboard.placeShip(selectedShip, [0, 0], [0, 10])).toThrow;
+});
+
+test("gameboard.placeShip() does not allow diagonals", () => {
+  const selectedShipArray = ship.getShips().filter((ship) => ship.id === "5");
+  const selectedShip = selectedShipArray[0];
+  expect(() => gameboard.placeShip(selectedShip, [0, 0], [4, 4])).toThrow;
+});
+
+test("The distance between the inputted coordinates in gameboard.placeShip() must equal the inputted ship's length", () => {
+  const selectedShipArray = ship.getShips().filter((ship) => ship.id === "5");
+  const selectedShip = selectedShipArray[0];
+  expect(() => gameboard.placeShip(selectedShip, [0, 0], [0, 5])).toThrow;
+  expect(() => gameboard.placeShip(selectedShip, [0, 0], [0, 3])).toThrow;
+  expect(() => gameboard.placeShip(selectedShip, [0, 0], [5, 0])).toThrow;
+});
+
+test("gameboard.placeShip() allows valid coordinates in any order, i.e. gameboard.placeShip(ship, [0,0], [0,4]) works the same as gameboard.placeShip(ship, [0,4], [0,0])", () => {
+  const selectedShipArray = ship.getShips().filter((ship) => ship.id === "5");
+  const selectedShip = selectedShipArray[0];
+  gameboard.placeShip(selectedShip, [0, 4], [0, 0]);
+  const board = gameboard.getBoard();
+  const firstRow = board[0];
+  const firstFiveSquares = firstRow.slice(0, 5);
+  firstFiveSquares.forEach((square) => {
+    expect(square).toBe("5");
+  });
+});
+
+test("gameboard.clearBoard() clears the board", () => {
+  const selectedShipArray = ship.getShips().filter((ship) => ship.id === "5");
+  const selectedShip = selectedShipArray[0];
+  gameboard.placeShip(selectedShip, [0, 0], [0, 4]);
+  gameboard.clearBoard();
+  const board = gameboard.getBoard();
+  board.forEach((row) => {
+    row.forEach((column) => {
+      expect(column).toBe(null);
+    });
   });
 });
