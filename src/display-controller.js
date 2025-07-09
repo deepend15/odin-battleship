cd0e145a5e6501ed373e5b91e8c02ba8c3143806
deepend15 <170750487+deepend15@users.noreply.github.com>
2025-07-09 17:22:59 -0400

    Create boards on the page via JavaScript
    -Create opponent & active player sections and boards on the webpage
    dynamically using JS, instead of in the HTML
    -Update all instances of "current" player to "active" player for logic
    & consistency purposes

--- src/display-controller.js
+++ src/display-controller.js
@@ -1,42 +1,136 @@
 import { game } from "./game.js";
 
 export const displayController = (function () {
-  const currentPlayerBoardDiv = document.querySelector(".current-player-board");
-  const opponentBoardDiv = document.querySelector(".opponent-board");
+  const gameDiv = document.querySelector(".game-div");
 
   const updateScreen = () => {
-    opponentBoardDiv.textContent = "";
-    currentPlayerBoardDiv.textContent = "";
+    // clear gameDiv
 
-    let currentPlayer;
-    let opponent;
+    gameDiv.textContent = "";
 
-    if (game.getActivePlayer().id === "Player 1") {
-      currentPlayer = game.getPlayer1();
-      opponent = game.getPlayer2();
-    } else {
-      currentPlayer = game.getPlayer2();
-      opponent = game.getPlayer1();
+    // get activePlayer and opponent
+
+    const activePlayer = game.getActivePlayer();
+    const opponent = game.getOpponent();
+
+    // create opponent board side of webpage
+
+    const opponentSection = document.createElement("div");
+    opponentSection.classList.add("opponent-section");
+    const opponentSectionH2 = document.createElement("h2");
+    opponentSectionH2.textContent = `${opponent.name}'s waters:`;
+    const generalBoardDiv1 = document.createElement("div");
+    generalBoardDiv1.classList.add("board-div");
+    const opponentColumnLabels = document.createElement("div");
+    opponentColumnLabels.classList.add("column-labels");
+    const columnLabelsText = "ABCDEFGHIJ".split("");
+    for (let i = 0; i < 10; i++) {
+      const p = document.createElement("p");
+      p.textContent = columnLabelsText[i];
+      opponentColumnLabels.append(p);
     }
+    const opponentRowLabels = document.createElement("div");
+    opponentRowLabels.classList.add("row-labels");
+    const rowLabelsText = "0123456789".split("");
+    for (let i = 0; i < 10; i++) {
+      const p = document.createElement("p");
+      p.textContent = rowLabelsText[i];
+      opponentRowLabels.append(p);
+    }
+    const opponentBoardDiv = document.createElement("div");
+    opponentBoardDiv.classList.add("opponent-board");
+    generalBoardDiv1.append(
+      opponentColumnLabels,
+      opponentRowLabels,
+      opponentBoardDiv,
+    );
+    opponentSection.append(opponentSectionH2, generalBoardDiv1);
+
+    // create active player board side of webpage
+
+    const activePlayerSection = document.createElement("div");
+    activePlayerSection.classList.add("active-player-section");
+    const activePlayerSectionH2 = document.createElement("h2");
+    activePlayerSectionH2.textContent = `${activePlayer.name}'s waters:`;
+    const generalBoardDiv2 = document.createElement("div");
+    generalBoardDiv2.classList.add("board-div");
+    const activePlayerColumnLabels = document.createElement("div");
+    activePlayerColumnLabels.classList.add("column-labels");
+    for (let i = 0; i < 10; i++) {
+      const p = document.createElement("p");
+      p.textContent = columnLabelsText.shift();
+      activePlayerColumnLabels.append(p);
+    }
+    const activePlayerRowLabels = document.createElement("div");
+    activePlayerRowLabels.classList.add("row-labels");
+    for (let i = 0; i < 10; i++) {
+      const p = document.createElement("p");
+      p.textContent = rowLabelsText.shift();
+      activePlayerRowLabels.append(p);
+    }
+    const activePlayerBoardDiv = document.createElement("div");
+    activePlayerBoardDiv.classList.add("active-player-board");
+    generalBoardDiv2.append(
+      activePlayerColumnLabels,
+      activePlayerRowLabels,
+      activePlayerBoardDiv,
+    );
+    activePlayerSection.append(activePlayerSectionH2, generalBoardDiv2);
+
+    // add opponentSection & activePlayerSection to gameDiv
+
+    gameDiv.append(opponentSection, activePlayerSection);
+
+    // fill in opponent board
 
     const opponentGameboard = opponent.gameboard;
     const opponentBoard = opponentGameboard.getBoard();
 
-    opponentBoard.forEach((row) => {
-      row.forEach((square) => {
+    opponentBoard.forEach((row, index) => {
+      let rowIndex = index;
+      row.forEach((square, index) => {
         const newSquare = document.createElement("div");
         newSquare.classList.add("square");
+        newSquare.dataset.row = rowIndex;
+        newSquare.dataset.column = index;
+        if (Array.isArray(square)) {
+          if (square.includes("miss")) newSquare.classList.add("miss");
+          if (square.includes("hit")) {
+            newSquare.classList.add("hit");
+            newSquare.textContent = "X";
+          }
+        }
         opponentBoardDiv.appendChild(newSquare);
       });
     });
 
-    const currentPlayerGameboard = currentPlayer.gameboard;
-    const currentPlayerBoard = currentPlayerGameboard.getBoard();
+    // add event listeners to opponent board cells
 
-    currentPlayerBoard.forEach((row) => {
-      row.forEach((square) => {
+    const opponentSquares = opponentBoardDiv.querySelectorAll(".square");
+
+    function toggleSquareHover(e) {
+      e.target.classList.toggle("hover");
+    }
+
+    opponentSquares.forEach((square) => {
+      if (square.textContent === "") {
+        square.addEventListener("mouseover", toggleSquareHover);
+        square.addEventListener("mouseout", toggleSquareHover);
+      }
+    });
+
+    // fill in active player board
+
+    const activePlayerGameboard = activePlayer.gameboard;
+    const activePlayerBoard = activePlayerGameboard.getBoard();
+
+    activePlayerBoard.forEach((row, index) => {
+      let rowIndex = index;
+      row.forEach((square, index) => {
         const newSquare = document.createElement("div");
         newSquare.classList.add("square");
+        newSquare.dataset.row = rowIndex;
+        newSquare.dataset.column = index;
         if (square !== null) {
           const ids = ["2", "3A", "3B", "4", "5"];
           ids.forEach((id) => {
@@ -46,10 +140,10 @@ export const displayController = (function () {
             }
           });
         }
-        currentPlayerBoardDiv.append(newSquare);
+        activePlayerBoardDiv.append(newSquare);
       });
     });
   };
 
-  return { updateScreen }
+  return { updateScreen };
 })();
