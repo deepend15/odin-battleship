9f3d7d64fd62f0733c7201dc54c863e0232a634e
deepend15 <170750487+deepend15@users.noreply.github.com>
2025-07-13 13:48:23 -0400

    Remove imported game module from index.js
    No longer needed with changes made to display-controller module.
    
    A couple of testing functions were added and commented out that allow
    for shortcuts to different game-states - these will be removed in the
    final code.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,48 @@
 import "./styles.css";
-import { game } from "./game.js";
 import { displayController } from "./display-controller.js";
 
-game.startGame("Bob");
 displayController.updateScreen();
+
+// function computerWinGame() {
+//   const player1 = game.getPlayer1();
+//   const player1Gameboard = player1.gameboard;
+//   const player1Board = player1Gameboard.getBoard();
+
+//   const ids = ["2", "3A", "3B", "4", "5"];
+
+//   player1Board.forEach((row, index) => {
+//     const rowIndex = index;
+//     row.forEach((square, index) => {
+//       if (Array.isArray(square)) {
+//         ids.forEach((id) => {
+//           if (square.includes(id)) {
+//             player1Gameboard.receiveAttack([rowIndex, index]);
+//           }
+//         });
+//       }
+//     });
+//   });
+
+//   player1.lastAttack = ["J", "10"];
+//   player1.lastAttackResult = "MISS";
+//   game.setGameStatus("game-over");
+//   displayController.updateScreen();
+// }
+
+// computerWinGame();
+
+// function computerSinkAShip() {
+//   const player1 = game.getPlayer1();
+//   const player1Gameboard = player1.gameboard;
+//   const computer = game.getPlayer2();
+
+//   player1Gameboard.receiveAttack([4, 0]);
+//   player1Gameboard.receiveAttack([4, 1]);
+
+//   computer.lastAttack = ["A", "5"];
+//   computer.lastAttackResult = "SINK";
+//   game.setGameStatus("computer-attack");
+//   displayController.updateScreen();
+// }
+
+// computerSinkAShip();
